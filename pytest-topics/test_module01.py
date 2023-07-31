ee67e04390869beedec97dd866b17f8ed130161a
Bui Duy Hieu (MS/ETA21) <bhu3hc@bosch.com>
2023-07-31 17:34:08 +0700

    change testcase final

--- pytest-topics/test_module01.py
+++ pytest-topics/test_module01.py
@@ -1,8 +1,8 @@
 def test_a1():
     assert 5 + 5 == 10
 
-def test_a2():
-    assert 9/5 == 1.5, "failed test intentionally"
+#def test_a2():
+#    assert 9/5 == 1.5, "failed test intentionally"
 
 def test_a3():
     assert 9//5 == 1
